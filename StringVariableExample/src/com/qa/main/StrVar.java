70248686f7fb5d6cc077ce440a527b967fdb9acf
DylanPatel11 <me@dylanpatel.co.uk>
2022-08-04 12:05:36 +0100

    String variable added in main: DP

---
+++ StringVariableExample/src/com/qa/main/StrVar.java
@@ -0,0 +1,11 @@
+package com.qa.main;
+
+public class StrVar {
+	
+	public static void main(String[] args) {
+		String message = "hello"; //identify what variable will be ie string int ect. then give value and recall in sysout
+		System.out.println(message);
+		
+	}
+
+}
