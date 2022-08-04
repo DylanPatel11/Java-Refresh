9e0fb189cc56d42329980568202fb321ef4c4816
DylanPatel11 <me@dylanpatel.co.uk>
2022-08-04 11:56:37 +0100

    HelloWorld added: DP

---
+++ HelloWorldExample/src/com/dp/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dp.helloworld;
+
+public class HelloWorld {
+	//main crtl+space and 1st option
+	public static void main(String[] args) { // alot of the time only have one main method
+		System.out.println("Hello World"); // to start a new line
+		System.out.print("here");
+	}
+
+}
