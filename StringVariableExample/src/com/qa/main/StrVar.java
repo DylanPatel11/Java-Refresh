71540d9b96f52d6da5a05b5f1417d2c01bb0dd12
DylanPatel11 <me@dylanpatel.co.uk>
2022-08-04 13:13:18 +0100

    DP: Return type method explained

--- StringVariableExample/src/com/qa/main/StrVar.java
+++ StringVariableExample/src/com/qa/main/StrVar.java
@@ -3,15 +3,15 @@ package com.qa.main;
 
 public class StrVar {
 	
-	public static void printMessage() {
-		String messages = "Hello, variable string message";
-		System.out.println(messages);
+	// change void method to return the type of data we want to return (return type method)
+	public static String printMessage() {
+		return "Hello World";
 	}
 	
+	//main method below
 	public static void main(String[] args) {
-		String message = "hello"; //identify what variable will be ie string int ect. then give value and recall in sysout
-		System.out.println(message);
-		printMessage();
+		System.out.println(printMessage()); // have to wrap printMessage() in a print method
+		
 		
 	}
 
